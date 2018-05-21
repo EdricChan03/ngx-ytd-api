dacb168ade83b3c6eb277038b6f743e43df09587
Edric Chan <edric.chan.1997@gmail.com>
2018-05-21 09:47:53 +0800

    chore(travis): lint and test demo as well

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,18 +12,27 @@ if [[ ! -z ${TRAVIS_BUILD_DIR+x} ]]; then
 	cd $TRAVIS_BUILD_DIR
 fi
 
-if [[ "$MODE" = "lint" ]]; then
-	# Lint mode
-	ng lint
-elif [[ "$MODE" = "test" ]]; then
-	# Test mode
-	ng test --watch=false
-fi
+case $MODE in
+	"lint-lib")
+		echo -e "\x1b[34mLinting the library...\x1b[0m"
+		ng lint ngx-ytd-api-lib;;
+	"test-lib")
+		echo -e "\x1b[34mTesting the library...\x1b[0m"
+		ng test ngx-ytd-api-lib --watch=false;;
+	"lint-demo")
+		echo -e "\x1b[34mLinting the demo app...\x1b[0m"
+		ng lint ngx-ytd-api-demo-e2e;;
+	"test-demo")
+		echo -e "\x1b[34mTesting the demo app...\x1b[0m"
+		ng test ngx-ytd-api-demo-e2e --watch=false;;
+esac
 
 # Deploying
-if [ "$DEPLOY_MODE" = "build-artifacts" ]; then
-	./scripts/publish-build-artifacts.sh
-elif [ "$DEPLOY_MODE" = "docs" ]; then
-	echo -e "\x1b[33m\x1b[1mWARN: Docs mode hasn't been implemented yet. Exiting...\x1b[0m"
-	exit 0
-fi
+case $DEPLOY_MODE in
+	"build-artifacts")
+		echo -e "\x1b[34mGenerating build artifacts...\x1b[0m"
+		./scripts/publish-build-artifacts.sh;;
+	"docs")
+		echo -e "\x1b[33m\x1b[1mWARN: Docs mode hasn't been implemented yet. Exiting...\x1b[0m"
+		exit 0;;
+esac
