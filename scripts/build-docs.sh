207e6c15597d7870f7f1797a15e807a727dbc826
Edric Chan <edric.chan.1997@gmail.com>
2018-06-02 23:50:37 +0800

    chore(travis): add debug stuff
    See why Firebase Hosting doesn't work..

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -13,10 +13,15 @@ set -e
 # Function to build the docs & demo
 #   @param ${1} The location to deploy to
 buildDocs() {
-	deployAt=${1}
+	deployAt="$1"
 	echo -e "\x1b[34mBuilding demo & docs...\x1b[0m"
+	echo -e "\x1b[34mDEBUG: Current directory: $TRAVIS_BUILD_DIR\x1b[0m"
 	ng build ngx-ytd-api-demo --prod --base-href "/ngx-ytd-api/${deployAt}/" --delete-output-path false --output-path "dist/ngx-ytd-api-demo/${deployAt}"
 	echo -e "\x1b[32mDone building.\x1b[0m"
+	ls $TRAVIS_BUILD_DIR/dist/ngx-ytd-api-demo
+	if [[ ! -d "$TRAVIS_BUILD_DIR/dist/ngx-ytd-api-demo" ]]; then
+		echo -e "\x1b[34mDEBUG: Build directory doesn't exist\x1b[0m"
+	fi
 	exit 0
 }
 
