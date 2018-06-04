960ac69f293bcea7d21f6ba3d7199588fd4ed593
Edric Chan <edric.chan.1997@gmail.com>
2018-06-04 17:56:56 +0800

    chore(travis): add variable

--- scripts/snapshot-docs.sh
+++ scripts/snapshot-docs.sh
@@ -18,7 +18,7 @@ buildVersionName="${buildVersion}-${commitSha}"
 buildTagName="${branchName}-${commitSha}"
 buildCommitMessage="${branchName} - ${commitMessage}"
 echo -e "\x1b[34m\x1b[1mBuilding docs site release...\x1b[0m"
-rm -rf dist
+rm -rf $TRAVIS_BUILD_DIR/dist
 ./scripts/build-docs.sh --generate-for-master
 
 echo -e "\x1b[34mDEBUG: Listing dist files...\x1b[0m"
@@ -27,16 +27,16 @@ ls ./dist/ngx-ytd-api-demo
 echo -e "\x1b[34mDEBUG: Current directory: $(pwd)\x1b[0m"
 
 echo -e "\x1b[34m\x1b[1mCloning ngx-ytd-api-demo-builds...\x1b[0m"
-rm -rf ./ngx-ytd-api-demo-builds
+rm -rf $TRAVIS_BUILD_DIR/ngx-ytd-api-demo-builds
 git clone https://github.com/Chan4077/ngx-ytd-api-demo-builds --depth=1
 
 echo -e "\x1b[34m\x1b[1mRemoving previous snapshot...\x1b[0m"
-rm -rf ./ngx-ytd-api-demo-builds/master
+rm -rf $TRAVIS_BUILD_DIR/ngx-ytd-api-demo-builds/master
 
 echo -e "\x1b[34m\x1b[1mCopying docs site to snapshot...\x1b[0m"
-cp -Rf ./dist/ngx-ytd-api-demo/master ./ngx-ytd-api-demo-builds/master
+cp -Rf $TRAVIS_BUILD_DIR/dist/ngx-ytd-api-demo/master $TRAVIS_BUILD_DIR/ngx-ytd-api-demo-builds/master
 
-cd ngx-ytd-api-demo-builds
+cd $TRAVIS_BUILD_DIR/ngx-ytd-api-demo-builds
 
 # GitHub token specified as Travis environment variable
 git config user.name "${commitAuthorName}"
@@ -51,9 +51,9 @@ git commit -m "snapshot: $buildVersionName"
 echo -e "\x1b[34m\x1b[1mPushing snapshot...\x1b[0m"
 git push -q origin master
 
-cd ..
+cd $TRAVIS_BUILD_DIR
 
 echo -e "\x1b[34m\x1b[1mCleaning up...\x1b[0m"
-rm -rf ngx-ytd-api-demo-builds
+rm -rf $TRAVIS_BUILD_DIR/ngx-ytd-api-demo-builds
 
 echo -e "\x1b[32m\x1b[1mSuccessfully pushed the snapshot to Chan4077/ngx-ytd-api-demo-builds!\x1b[0m"
