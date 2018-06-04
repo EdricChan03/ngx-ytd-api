a2ea31bce8047bedd14730807d7a876af1b1779c
Edric Chan <edric.chan.1997@gmail.com>
2018-06-04 16:41:33 +0800

    fix(travis): correct escape sequence

--- scripts/snapshot-docs.sh
+++ scripts/snapshot-docs.sh
@@ -28,7 +28,7 @@ git clone https://github.com/Chan4077/ngx-ytd-api-demo-builds --depth=1
 echo -e "\x1b[34m\x1b[1mRemoving previous snapshot...\x1b[0m"
 rm -rf ngx-ytd-api-demo-builds/master
 
-echo -e "\x1b[34m\x1b[1mCopying docs site to snapshot...\xb1[0m"
+echo -e "\x1b[34m\x1b[1mCopying docs site to snapshot...\x1b[0m"
 cp -Rf dist/ngx-ytd-api-demo/master ngx-ytd-api-demo-builds/master
 
 cd ngx-ytd-api-demo-builds
