4fd2051a8007fbe08b657421fe898332e10256ea
Edric Chan <edric.chan.1997@gmail.com>
2018-06-05 10:41:03 +0800

    fix(travis): use `-n` instead of `-v`

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -8,7 +8,8 @@ CLICOLOR=1
 set -e
 
 # Go to project directory
-if [[ -v TRAVIS_BUILD_DIR ]]; then
+if [[ -n "$TRAVIS_BUILD_DIR" ]]; then
+	echo -e "\x1b[34mChanging to the build directory...\x1b[0m"
 	cd $TRAVIS_BUILD_DIR
 fi
 
