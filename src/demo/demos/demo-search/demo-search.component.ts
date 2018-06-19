bfb03d579b83fe5ead531d778263b93e20d4cbd8
Edric Chan <edric.chan.1997@gmail.com>
2018-06-19 20:45:51 +0800

    chore(demo): add `videoDimension` property

--- src/demo/demos/demo-search/demo-search.component.ts
+++ src/demo/demos/demo-search/demo-search.component.ts
@@ -58,13 +58,13 @@ export class DemoSearchComponent {
 		this.searchForm = fb.group({
 			query: ['', Validators.required],
 			key: ['', [Validators.required, Validators.maxLength(39), Validators.minLength(39)]],
-			embeddable: false,
 			maxResults: [50, [Validators.required, Validators.min(0), Validators.max(50)]],
 			type: ['video,channel,playlist', Validators.required],
 			videoOptions: fb.group({
 				videoCaption: 'any',
 				videoCategoryId: '',
 				videoDefinition: 'any',
+				videoDimension: 'any',
 				videoDuration: 'any',
 				videoEmbeddable: 'any',
 				videoLicense: 'any',
@@ -96,17 +96,28 @@ export class DemoSearchComponent {
 		this.searchForm.reset();
 	}
 	search(pageToken?: string) {
-		const _apiConfig = {
+		const _apiConfig: NgxYtdApiSearchOpts = {
 			key: ''
 		};
 		const _rawValue = this.searchForm.getRawValue();
 		for (const prop in _rawValue) {
 			if (typeof _rawValue[prop] === 'object') {
+				const _rawValueObj = _rawValue[prop];
+				if (this.isVideoType) {
+					// tslint:disable-next-line:forin
+					for (const objProp in _rawValueObj) {
+						// console.log(_rawValueObj[objProp])
+						_apiConfig[objProp] = _rawValueObj[objProp];
+					}
+				}
 				// Assume object is `videoOptions`
 				console.log(_rawValue[prop]);
 			} else {
 				_apiConfig[prop] = _rawValue[prop];
 			}
+		}
+		if (pageToken) {
+			_apiConfig.pageToken = pageToken;
 		}
 		// const _apiConfig: NgxYtdApiSearchOpts = this.searchForm.getRawValue();
 		console.log(this.searchForm.getRawValue());
