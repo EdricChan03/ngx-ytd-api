91188132fa825717442a3862e9222c7751e29d78
Edric Chan <edric.chan.1997@gmail.com>
2018-07-14 10:38:20 +0800

    feat(lib): add initial video API

---
+++ src/lib/videos/public_api.ts
@@ -0,0 +1,3 @@
+export * from './ytd-api-videos.interface';
+
+export * from './ytd-api-videos.service';
---
+++ src/lib/videos/ytd-api-videos.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { NgxYtdApiVideosListOpts, NgxYtdApiVideosListResult } from './ytd-api-videos.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class NgxYtdApiVideoService {
+	readonly ytdApiVideoBaseApiUrl = 'https://www.googleapis.com/youtube/v3/videos';
+
+	constructor(private http: HttpClient) { }
+	/**
+	 * Returns a list of videos that match the requested API parameters
+	 * @param opts API parameters for the API
+	 */
+	list(opts: NgxYtdApiVideosListOpts): Observable<NgxYtdApiVideosListResult> {
+		let _hasPartOpt = false;
+		let _apiUrl = `${this.ytdApiVideoBaseApiUrl}`;
+		// Loop through every property in the opts object
+		for (const prop in opts) {
+			// Check if property has a non-null value
+			if (opts.hasOwnProperty(prop) && opts[prop] !== null) {
+				// Add parameter to the API URL
+				_apiUrl += `&${prop}=${encodeURI(opts[prop])}`;
+				if (prop === 'part') {
+					_hasPartOpt = true;
+				}
+			}
+		}
+		// Default if `part` parameter isn't specified
+		if (!_hasPartOpt) {
+			_apiUrl += '&part=snippet,id';
+		}
+		return this.http.get<NgxYtdApiVideosListResult>(_apiUrl);
+	}
+}
