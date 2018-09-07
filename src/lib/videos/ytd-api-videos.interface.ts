f0a504d9d8a081065928de3f1d8b05469ac394f5
Edric Chan <edric.chan.1997@gmail.com>
2018-09-07 14:34:20 +0800

    chore(lib/videos): add more options for `NgxYtdApiVideos#list`

--- src/lib/videos/ytd-api-videos.interface.ts
+++ src/lib/videos/ytd-api-videos.interface.ts
@@ -1,11 +1,14 @@
 import { NgxYtdApiGenericOpts, NgxYtdApiGenericResult } from 'ngx-ytd-api/core';
 
+/**
+ * Options for {@link NgxYtdApiVideoService#list}
+ */
 export interface NgxYtdApiVideosListOpts extends NgxYtdApiGenericOpts {
   /**
    * Identifies the chart that should be retrieved.
    * Acceptable values:
    * - `mostPopular`: Return the most popular videos for the specified content region and video category.
-   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#chart|developer docs} for more info
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#chart|developer docs} for more info.
    * Note: This parameter is a filter and only one filter is allowed
    */
   chart?: 'mostPopular';
@@ -24,6 +27,59 @@ export interface NgxYtdApiVideosListOpts extends NgxYtdApiGenericOpts {
    * Note: This parameter is a filter and only one filter is allowed
    */
   myRating?: 'dislike' | 'like';
+  /**
+   * Specifies the API to return localized resource metadata for a specific language that the YouTube website supports.
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#hl|developer docs} for more info.
+   */
+  hl?: string;
+  /**
+   * Specifies the maximum height of the embedded YouTube player returned in the `player.embedHtml` property.
+   * This parameter can be used to specify a height appropriate for your app's layout.
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#maxHeight|developer docs} for more info
+   * Note: If the `maxWidth` property is also specified, the player may be shorter
+   * than the `maxHeight` in order to not violate the maximum width.
+   * Acceptable values are 72 to 8192, inclusive.
+   */
+  maxHeight?: number;
+  /**
+   * Specifies the maximum results that should be returned.
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#maxResults|developer docs} for more info
+   * Note: This parameter is to be used for the `myRating` parameter, but should not
+   * be used with the `id` parameter.
+   * Acceptable values: 1 to 50, inclusive. 5 is the default number.
+   */
+  maxResults?: number;
+  /**
+   * Specifies the maximum width of the embedded YouTube player returned in the `player.embedHtml` property.
+   * This parameter can be used to specify a width appropriate for your app's layout.
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#maxWidth|developer docs} for more info
+   * Note: If the `maxHeight` property is also specified, the player may be shorter
+   * than the `maxWidth` in order to not violate the maximum width.
+   * Acceptable values are 72 to 8192, inclusive.
+   */
+  maxWidth?: number;
+  /**
+   * Identifies a specific page in the result set that should be returned.
+   *
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#pageToken|developer docs} for more info
+   * Note: This parameter is to be used with the `myRating` parameter, but is not to be used
+   * with the `id` parameter.
+   */
+  pageToken?: string;
+  /**
+   * Tells the API to select a video chart available in the specific region.
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#regionCode|developer docs} for more info
+   * Note: This parameter is to only be used with the `chart` parameter.
+   */
+  regionCode?: string;
+  /**
+   * This property identifies the video category for which the video should be retrieved.
+   * See the {@link https://developers.google.com/youtube/v3/docs/videos/list#regionCode|developer docs} for more info
+   * Note: This parameter is to only be used with the `chart` parameter.
+   * By default, charts are not restricted to a particular category.
+   * The default value is 0.
+   */
+  videoCategoryById?: string;
 }
 
 export interface NgxYtdApiVideosListResultItemSuggestionTagSuggestion {
