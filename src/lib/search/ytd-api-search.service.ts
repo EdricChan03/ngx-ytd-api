fe6963295f6cd0b1982bbed9057b4142c0179883
Edric Chan <edric.chan.1997@gmail.com>
2018-09-07 14:42:19 +0800

    chore(lib/search): remove `search` method
    BREAKING CHANGE:
    `NgxYtdApiSearch#search` has been removed in favour of `NgxYtdApiSearch#list` for aligning with YouTube Data v3 Search API.

--- src/lib/search/ytd-api-search.service.ts
+++ src/lib/search/ytd-api-search.service.ts
@@ -21,18 +21,6 @@ export class NgxYtdApiSearchService {
    * @param query The query to search for. See the {@link https://developers.google.com/youtube/v3/docs/search/list#q|developer docs}
    * for more info.
    * @param opts Options for searching
-   * @deprecated Use {@link NgxYtdApiSearchService#list} instead.
-   * @deletion-target 1.0.0-alpha.5
-   * @returns Results of the search as a stream
-   */
-  search(query: string, opts: NgxYtdApiSearchListOpts): Observable<NgxYtdApiSearchListResult> {
-    return this.list(query, opts);
-  }
-  /**
-   * Searches using the YouTube HTTP Data API
-   * @param query The query to search for. See the {@link https://developers.google.com/youtube/v3/docs/search/list#q|developer docs}
-   * for more info.
-   * @param opts Options for searching
    * @returns Results of the search as a stream
    */
   list(query: string, opts: NgxYtdApiSearchListOpts): Observable<NgxYtdApiSearchListResult> {
