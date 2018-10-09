c7d75b715c1cabbc7d4508408d2bee62d9038642
Edric Chan <edric.chan.1997@gmail.com>
2018-10-09 18:07:56 +0800

    feat(lib): add support for standard query parameters
    See https://developers.google.com/youtube/v3/docs/standard_parameters for more info.
    Closes #31

--- src/lib/core/ytd-api.interfaces.ts
+++ src/lib/core/ytd-api.interfaces.ts
@@ -3,6 +3,20 @@
  */
 export interface NgxYtdApiGenericOpts {
   /**
+   * The access token from Google's OAuth 2.0 API
+   * This is to be passed to the `Authorization` HTTP header
+   */
+  accessToken?: string;
+  /**
+   * The callback function.
+   */
+  callback?: string;
+  /**
+   * Specifies which fields to return
+   * See https://developers.google.com/youtube/v3/getting-started#partial for more info
+   */
+  fields?: string;
+  /**
    * The API key for accessing the API
    */
   key?: string;
@@ -12,19 +26,30 @@ export interface NgxYtdApiGenericOpts {
    */
   part?: string;
   /**
-   * The access token from Google's OAuth 2.0 API
-   * This is to be passed to the `Authorization` HTTP header
+   * Whether to return the response with line breaks & indentations
    */
-  accessToken?: string;
+  prettyPrint?: boolean;
+  /**
+   * Gives the ability to enforce per-user quotas from a server-side application even
+   * in cases when the user's IP address is unknown.
+   * See https://developers.google.com/youtube/v3/docs/standard_parameters#quotaUser for more info
+   */
+  quotaUser?: string;
+  /**
+   * Gives the ability to enforce per-user quotas when calling the API from a server-side
+   * application.
+   * See https://developers.google.com/youtube/v3/docs/standard_parameters#userIp for more info
+   */
+  userIp?: string;
 }
 
 export interface NgxYtdApiGenericResult {
   /**
-   * The API's resource type
-   */
-  kind?: string;
-  /**
    * The ETag of the request
    */
   etag?: string;
+  /**
+   * The API's resource type
+   */
+  kind?: string;
 }
