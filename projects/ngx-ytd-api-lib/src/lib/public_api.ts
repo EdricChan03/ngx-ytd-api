9c884815c83f68e734c310413c34cba474f683d7
Edric Chan <edric.chan.1997@gmail.com>
2018-10-17 21:27:46 +0800

    chore: fix imports

--- projects/ngx-ytd-api-lib/src/lib/public_api.ts
+++ projects/ngx-ytd-api-lib/src/lib/public_api.ts
@@ -3,7 +3,7 @@ Allow developers to import from `ngx-ytd-api` instead of a submodule.
 Note that this may functionality may be removed in a future release.
 */
 
-export * from './core';
-export * from './comments';
-export * from './search';
-export * from './videos';
+export * from 'ngx-ytd-api/core';
+export * from 'ngx-ytd-api/comments';
+export * from 'ngx-ytd-api/search';
+export * from 'ngx-ytd-api/videos';
