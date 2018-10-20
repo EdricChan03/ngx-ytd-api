48553fa52cdf99e915f9597c02879c184b9588e6
Edric Chan <edric.chan.1997@gmail.com>
2018-10-20 21:31:03 +0800

    fix(lib/core): correct API endpoint of `ngxYtdCommentsApiUrl`

--- projects/ngx-ytd-api-lib/src/lib/core/ytd-api-core.service.ts
+++ projects/ngx-ytd-api-lib/src/lib/core/ytd-api-core.service.ts
@@ -13,7 +13,7 @@ export class NgxYtdApiCoreService {
   /**
    * The API URL for the Comments API of the YouTube Data v3 API
    */
-  readonly ngxYtdCommentsApiUrl = 'https://www.googleapis.com/youtube/v3/search';
+  readonly ngxYtdCommentsApiUrl = 'https://www.googleapis.com/youtube/v3/comments';
   /**
    * The API URL for the Search API of the YouTube Data v3 API
    */
