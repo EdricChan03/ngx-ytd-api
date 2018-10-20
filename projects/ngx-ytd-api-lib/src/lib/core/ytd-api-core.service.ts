549fc43b9a3dbc97984eddf3565a142e4283c02a
Edric Chan <edric.chan.1997@gmail.com>
2018-10-20 21:50:18 +0800

    fix(lib/core): remove `private` tag

--- projects/ngx-ytd-api-lib/src/lib/core/ytd-api-core.service.ts
+++ projects/ngx-ytd-api-lib/src/lib/core/ytd-api-core.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-/**
- * @private
- */
 @Injectable({
   providedIn: 'root'
 })
