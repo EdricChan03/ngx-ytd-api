72220c1e16dadcdce16e75203f6e600ef79a11df
Edric Chan <edric.chan.1997@gmail.com>
2018-10-26 19:36:29 +0800

    feat(core): introduce more HTTP request methods
    This commit introduces the following new methods:
    - `createHttpDelete`: Creates a HTTP DELETE request
    - `createHttpPost`: Creates a HTTP POST request
    - `createHttpPut`: Creates a HTTP PUT request
    This commits also adds a null check to check if a parameter is null

--- projects/ngx-ytd-api-lib/src/lib/core/ytd-api-core.service.ts
+++ projects/ngx-ytd-api-lib/src/lib/core/ytd-api-core.service.ts
@@ -19,14 +19,52 @@ export class NgxYtdApiCoreService {
    */
   readonly ngxYtdVideosApiUrl = 'https://www.googleapis.com/youtube/v3/videos';
   /**
-   * Generates the query parameters to be used with the API endpoint
+   * Generates the query parameters to be used for a HTTP DELETE request
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to be appended to the API endpoint
+   * @return A `HttpClient` instance
+   */
+  createHttpDelete<P extends any>(apiEndpoint: string, apiParams: P): Observable<any> {
+    let params = new HttpParams();
+    let headers;
+    for (const prop in apiParams) {
+      if (prop in apiParams && typeof apiParams[prop] !== undefined && apiParams[prop] !== null) {
+        if (prop === 'accessToken') {
+          headers = new HttpHeaders()
+            .set('Authorization', `Bearer ${apiParams[prop]}`);
+        } else {
+          params = params.append(prop, apiParams[prop]);
+        }
+      }
+    }
+    if (headers) {
+      return this.http.delete(
+        apiEndpoint,
+        {
+          params: params,
+          headers: headers
+        }
+      );
+    } else {
+      return this.http.delete(
+        apiEndpoint,
+        {
+          params: params
+        }
+      );
+    }
+  }
+  /**
+   * Generates the query parameters to be used for a HTTP GET request
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to be appended to the API endpoint
    * @return A `HttpClient` instance
    */
   createHttpGet<R, P extends any>(apiEndpoint: string, apiParams: P): Observable<R> {
     let params = new HttpParams();
     let headers;
     for (const prop in apiParams) {
-      if (prop in apiParams && typeof apiParams[prop] !== undefined) {
+      if (prop in apiParams && typeof apiParams[prop] !== undefined && apiParams[prop] !== null) {
         if (prop === 'accessToken') {
           headers = new HttpHeaders()
             .set('Authorization', `Bearer ${apiParams[prop]}`);
@@ -47,8 +85,85 @@ export class NgxYtdApiCoreService {
       return this.http.get<R>(
         apiEndpoint,
         {
+          params: params
+        }
+      );
+    }
+  }
+  /**
+   * Generates the query parameters to be used with the API endpoint
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to be appended to the API endpoint
+   * @param apiBody The request body
+   * @return A `HttpClient` instance
+   */
+  createHttpPost<R, P extends any>(apiEndpoint: string, apiParams: P, apiBody: any): Observable<R> {
+    let params = new HttpParams();
+    let headers;
+    for (const prop in apiParams) {
+      if (prop in apiParams && typeof apiParams[prop] !== undefined && apiParams[prop] !== null) {
+        if (prop === 'accessToken') {
+          headers = new HttpHeaders()
+            .set('Authorization', `Bearer ${apiParams[prop]}`);
+        } else {
+          params = params.append(prop, apiParams[prop]);
+        }
+      }
+    }
+    if (headers) {
+      return this.http.post<R>(
+        apiEndpoint,
+        apiBody,
+        {
           params: params,
           headers: headers
+        }
+      );
+    } else {
+      return this.http.post<R>(
+        apiEndpoint,
+        apiBody,
+        {
+          params: params
+        }
+      );
+    }
+  }
+  /**
+   * Generates the query parameters to be used with the API endpoint
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to be appended to the API endpoint
+   * @param apiBody The request body
+   * @return A `HttpClient` instance
+   */
+  createHttpPut<R, P extends any>(apiEndpoint: string, apiParams: P, apiBody: any): Observable<R> {
+    let params = new HttpParams();
+    let headers;
+    for (const prop in apiParams) {
+      if (prop in apiParams && typeof apiParams[prop] !== undefined && apiParams[prop] !== null) {
+        if (prop === 'accessToken') {
+          headers = new HttpHeaders()
+            .set('Authorization', `Bearer ${apiParams[prop]}`);
+        } else {
+          params = params.append(prop, apiParams[prop]);
+        }
+      }
+    }
+    if (headers) {
+      return this.http.put<R>(
+        apiEndpoint,
+        apiBody,
+        {
+          params: params,
+          headers: headers
+        }
+      );
+    } else {
+      return this.http.put<R>(
+        apiEndpoint,
+        apiBody,
+        {
+          params: params
         }
       );
     }
