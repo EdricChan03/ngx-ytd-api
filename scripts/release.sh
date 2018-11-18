4aa12466e6e75fa5d0d70ae3527b8d9be1d5058c
Edric Chan <edric.chan.1997@gmail.com>
2018-11-18 15:04:32 +0800

    build(release): add full support for releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,23 +13,32 @@ function checkCommandExists {
     echo -e "\x1b[34mDEBUG: Checking if the $1 command exists...\x1b[0m"
   fi
   if ! [[ -x "$(command -v $1)" ]]; then
-    echo -e "\x1b[31mERROR: The $1 command doesn't exist. Aborting...\x1b[0m"
+    echo -e "\x1b[31m\x1b[1mERROR: The $1 command doesn't exist. Aborting...\x1b[0m"
     if [[ -n "$3" ]]; then
-      echo -e "\x1b[31mReason: $3\x1b[0m"
+      echo -e "\x1b[31m\x1b[1mReason: $3\x1b[0m"
     fi
     return 1
   else
     if [[ "$2" = "true" ]]; then
-      echo -e "\x1b[33mDEBUG: The $1 command exists!\x1b[0m"
+      echo -e '\x1b[33mDEBUG: The $1 command exists!\x1b[0m'
     fi
     return
   fi
 }
 SHOW_DEBUG=${SHOW_DEBUG:-true}
+
+echo -e "\x1b[34mChecking if the project is clean (aka no uncommitted changes)...\x1b[0m"
+if [[ -n $(git status --porcelain) ]]; then
+  echo -e "\x1b[31m\x1b[1mERROR: The current project is dirty (aka there are uncommitted changes).\x1b[0m"
+  echo -e "\x1b[31m\x1b[1mA clean project is required to execute this script. Exiting...\x1b[0m"
+  exit 1
+fi
+
 echo -e "\x1b[34mChecking if the required commands exist...\x1b[0m"
 
-checkCommandExists "npm" "$SHOW_DEBUG"
+checkCommandExists "npm" "$SHOW_DEBUG" "The npm command is needed to update the package.json file."
 checkCommandExists "jq" "$SHOW_DEBUG" "The jq command is needed to modify the firebase.json file to add routing.\nTIP: Go here for more information on how to download the jq command: https://stedolan.github.io/jq/download/"
+checkCommandExists "gulp" "$SHOW_DEBUG" "The gulp command is needed to run the changelog script.\nTIP: Run npm i to install all dependencies."
 
 cd $(pwd)/projects/ngx-ytd-api-lib
 
@@ -49,7 +58,7 @@ if [[ $* == *--releaseType* ]]; then
 elif [[ -n "$RELEASE_TYPE" ]]; then
   npm version "$RELEASE_TYPE"
 else
-  echo -e "\x1b[31mERROR: Please specify a release type (via the --releaseType parameter) to bump the version!\x1b[0m"
+  echo -e '\x1b[31m\x1b[1mERROR: Please specify a release type (via the --releaseType parameter) to bump the version!\x1b[0m'
   exit 1
 fi
 
@@ -60,7 +69,7 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-echo -e "\x1b[33mSuccessfully bumped from $PACKAGE_PREV_VERSION to $PACKAGE_VERSION!\x1b[0m"
+echo -e '\x1b[33mSuccessfully bumped from $PACKAGE_PREV_VERSION to $PACKAGE_VERSION!\x1b[0m'
 
 # Go back to the project's root
 cd ../../
@@ -93,10 +102,18 @@ if [[ -e firebase.json ]]; then
 fi
 
 # GitHub token specified as Travis environment variable
-git config user.name "${commitAuthorName}"
-git config user.email "${commitAuthorEmail}"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${NGX_YTD_API_BUILDS_TOKEN}:@github.com" >.git/credentials
+if [[ "$TRAVIS" = true ]]; then
+  commitAuthorName=$(git --no-pager show -s --format='%an' HEAD)
+  commitAuthorEmail=$(git --no-pager show -s --format='%ae' HEAD)
+  git config user.name "${commitAuthorName}"
+  git config user.email "${commitAuthorEmail}"
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://$NGX_YTD_API_BUILDS_TOKEN:@github.com" >.git/credentials
+else
+  if [[ "$SHOW_DEBUG" = true ]]; then
+    echo -e "\x1b[34mDEBUG: Script is not being run on TravisCI. Assuming user's credentials.\x1b[0m"
+  fi
+fi
 
 echo -e "\x1b[34mCommiting release...\x1b[0m"
 git add -A
@@ -107,13 +124,11 @@ git push -q origin master
 
 cd ..
 
-echo -e "\x1b[34mCleaning up...\x1b[0m"
-rm -rf ngx-ytd-api-demo-builds
-
 echo -e "\x1b[32mSuccessfully pushed the release to Chan4077/ngx-ytd-api-demo-builds.\x1b[0m"
 
 cd dist/ngx-ytd-api-lib
-echo -e "\x1b[32mPublishing library to NPM...\x1b[0m"
+
+echo -e "\x1b[34mPublishing library to NPM...\x1b[0m"
 if [[ $* == *--publishNext* ]]; then
   npm publish --tag next
 else
@@ -122,5 +137,25 @@ fi
 
 cd ../../
 
-echo -e "\x1b[32mDone!\x1b[0m"
+echo -e "\x1b[34mGenerating changelog...\x1b[0m"
+gulp changelog
+
+echo -e "\x1b[34mCommitting changes...\x1b[0m"
+git add CHANGELOG.md projects/ngx-ytd-api-lib/package.json
+git commit -m "chore: bump version to $PACKAGE_VERSION w/ changelog"
+
+echo -e "\x1b[34mCreating tag...\x1b[0m"
+git tag "$PACKAGE_VERSION"
+
+echo -e "\x1b[34mPushing changes...\x1b[0m"
+git push -q origin master
+git push -q origin "$PACKAGE_VERSION"
+
+echo -e "\x1b[32mSuccessfully pushed the release to Chan4077/ngx-ytd-api.\x1b[0m"
+
+echo -e "\x1b[34mCleaning up...\x1b[0m"
+rm -rf ngx-ytd-api-demo-builds
+rm -rf dist
+
+echo -e '\x1b[32mDone!\x1b[0m'
 exit 0
