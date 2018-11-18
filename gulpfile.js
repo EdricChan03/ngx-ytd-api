cc7fb3f6e5e9dea64a7c19dd5a32231b587ae8bf
Edric Chan <edric.chan.1997@gmail.com>
2018-11-18 14:21:39 +0800

    chore(gulpfile): add support for release name

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 const conventionalChangelog = require('gulp-conventional-changelog');
+const { prompt } = require('inquirer');
 
 // Generates a changelog for the latest git tag
 gulp.task('changelog', () => {
+  const { releaseName } = await prompt({
+    type: 'text',
+    name: 'releaseName',
+    message: 'What should be the name for the release?'
+  });
   return gulp.src('CHANGELOG.md')
     .pipe(conventionalChangelog({
       preset: 'angular',
       // Regenerate everything
       // releaseCount: 0
-    }))
+    }, { title: releaseName }))
     .pipe(gulp.dest('./'));
 })
 // Cleans the changelog and regenerates it for all tags
