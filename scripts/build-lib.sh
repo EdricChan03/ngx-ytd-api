b5515fefa00738c7ff425c73e2e38d17ef477f89
Edric Chan <edric.chan.1997@gmail.com>
2018-12-22 22:39:35 +0800

    build(build-lib): remove support for `--dryRun` flag

--- scripts/build-lib.sh
+++ scripts/build-lib.sh
@@ -13,42 +13,43 @@ SKIP_NPM=false
 set -e
 # Arguments
 while
-  [[ $# -gt 0 ]]
+[[ $# -gt 0 ]]
 do
   opt="$1"
   shift # Expose the next argument
   case "$opt" in
-  # Publish to the `next` tag on NPM
-  "--publish-next" | "--publishNext")
-    PUBLISH_NEXT=true
+    # Publish to the `next` tag on NPM
+    "--publish-next" | "--publishNext")
+      PUBLISH_NEXT=true
     ;;
-  # Prevent builds from being published to NPM
-  "--dry-run" | "--dryRun")
-    DRY_RUN=true
+    # Prevent builds from being published to NPM
+    "--dry-run" | "--dryRun")
+      echo -e "\x1b[33mWARN: The --dryRun command has been deprecated in favour of --skipNpm.\x1b[0m"
+      SKIP_NPM=true
     ;;
-  # Whether this script was executed from `scripts.sh`
-  "--from-scripts")
-    FROM_SCRIPTS=true
+    # Whether this script was executed from `scripts.sh`
+    "--from-scripts")
+      FROM_SCRIPTS=true
     ;;
-  # Whether to skip publishing to NPM altogether
-  "--skip-npm" | "--skipNpm" | "--skipNPM")
-    SKIP_NPM=true
+    # Whether to skip publishing to NPM altogether
+    "--skip-npm" | "--skipNpm" | "--skipNPM")
+      SKIP_NPM=true
     ;;
-  # The version of the library to build
-  "--version")
-    VERSION="$1"
-    shift
+    # The version of the library to build
+    "--version")
+      VERSION="$1"
+      shift
     ;;
-  "--help" | "-h")
-    if [[ "$FROM_SCRIPTS" == true ]]; then
-      echo -e "\x1b[33mSyntax: ./scripts.sh (build-script | build) [--publishNext | --publish-next | --dry-run | --dryRun | --skipNpm | --help | -h]\x1b[0m"
-    else
-      echo -e "\x1b[33mSyntax: ./build-lib.sh [--publishNext | --publish-next | --dry-run | --dryRun | --skipNpm | --help | -h]\x1b[0m"
-    fi
-    exit 0
+    "--help" | "-h")
+      if [[ "$FROM_SCRIPTS" == true ]]; then
+        echo -e "\x1b[33mSyntax: ./scripts.sh (build-script | build) [--publishNext | --publish-next | --dry-run | --dryRun | --skipNpm | --help | -h]\x1b[0m"
+      else
+        echo -e "\x1b[33mSyntax: ./build-lib.sh [--publishNext | --publish-next | --skipNpm | --help | -h]\x1b[0m"
+      fi
+      exit 0
     ;;
-  *)
-    INVALID_ARGS+=($opt)
+    *)
+      INVALID_ARGS+=($opt)
     ;;
   esac
 done
@@ -81,25 +82,23 @@ else
         exit 1
       else
         echo -e "\x1b[32mDone copying files.\x1b[0m"
-        if [[ $SKIP_NPM == false ]]; then
-          if [[ $DRY_RUN == false ]]; then
-            cd dist/ngx-ytd-api-lib
-            # Check if NPM exists
-            if [[ ! -x $(type -P npm >/dev/null) ]] && [[ ! -x $(command -v npm) ]]; then
-              echo -e "\x1b[31mNPM is not installed. Please visit https://nodejs.org to get the latest package for your OS.\x1b[0m"
-              exit 1
-            else
-              if [[ $PUBLISH_NEXT = true ]]; then
-                npm publish --tag next
-              else
-                npm publish
-              fi
-            fi
+        if [[ $DRY_RUN == false ]]; then
+          cd dist/ngx-ytd-api-lib
+          # Check if NPM exists
+          if [[ ! -x $(type -P npm >/dev/null) ]] && [[ ! -x $(command -v npm) ]]; then
+            echo -e "\x1b[31mNPM is not installed. Please visit https://nodejs.org to get the latest package for your OS.\x1b[0m"
+            exit 1
           else
-            echo -e "\x1b[33mDry run has been enabled. Files will not be published to the NPM registry.\x1b[0m"
-            echo -e "\x1b[32mDone executing.\x1b[0m"
-            exit 0
+            if [[ $PUBLISH_NEXT = true ]]; then
+              npm publish --tag next
+            else
+              npm publish
+            fi
           fi
+        else
+          echo -e "\x1b[33mDry run has been enabled. Files will not be published to the NPM registry.\x1b[0m"
+          echo -e "\x1b[32mDone executing.\x1b[0m"
+          exit 0
         fi
       fi
     fi
