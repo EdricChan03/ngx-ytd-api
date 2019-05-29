a215cdc2966f8dcc1f2f8a22a543c5be6b1f6e76
Edric Chan <edric.chan.1997@gmail.com>
2019-05-29 10:36:17 +0800

    refactor(demo/version-picker): use menu items instead of navigation list

--- projects/ngx-ytd-api-demo/src/app/shared/version-picker/version-picker.component.ts
+++ projects/ngx-ytd-api-demo/src/app/shared/version-picker/version-picker.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { VERSION as ngxYtdApiVersion } from 'ngx-ytd-api';
+import { VERSION as ngxYtdApiVersion } from 'ngx-ytd-api/core';
 import { environment } from '../../../environments/environment';
 
 export interface VersionFile {
   versions: VersionMenuItem[];
 }
 export interface VersionMenuItem {
-  type: 'version' | 'subheader' | 'divider' | 'dividerInset';
   isSelected?: boolean;
   name?: string;
   link?: string;
@@ -27,36 +26,27 @@ export class VersionPickerComponent implements OnInit {
   ngOnInit() {
     if (environment.production) {
       this.http.get<VersionFile>('/versions.json').subscribe(result => {
+        const masterVersionMenuItem = this.versions.versions.find(item => item.name === 'MASTER_VERSION');
+        masterVersionMenuItem.name = this.currentVersion;
         this.versions = result;
       });
     } else {
       this.versions = {
         versions: [
           {
-            type: 'version',
             name: this.currentVersion,
             link: '/',
             isSelected: true
           },
           {
-            type: 'divider'
-          },
-          {
-            type: 'subheader',
-            name: 'Other versions'
-          },
-          {
-            type: 'version',
             name: '0.0.0-alpha.0',
             link: '/'
           },
           {
-            type: 'version',
             name: '0.0.0-beta.0',
             link: '/'
           },
           {
-            type: 'version',
             name: '0.0.0-rc.0',
             link: '/'
           }
