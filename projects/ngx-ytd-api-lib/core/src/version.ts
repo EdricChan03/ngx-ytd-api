ef55ec7a6236da68c02e3b14c5658be282352576
Edric Chan <edric.chan.1997@gmail.com>
2019-05-29 10:35:56 +0800

    refactor(lib): move VERSION constant to core submodule
    Probably won't break any apps that import the VERSION constant as it is still exported at the root

--- projects/ngx-ytd-api-lib/core/src/version.ts
+++ projects/ngx-ytd-api-lib/core/src/version.ts
@@ -1,3 +1,4 @@
 import { Version } from '@angular/core';
 
+/** Version of ngx-ytd-api */
 export const VERSION = new Version('0.0.0-PLACEHOLDER');
--- projects/ngx-ytd-api-lib/src/lib/public_api.ts
+++ projects/ngx-ytd-api-lib/src/lib/public_api.ts
@@ -1,8 +1,8 @@
 export * from 'ngx-ytd-api/comments';
+export * from 'ngx-ytd-api/core';
 export * from 'ngx-ytd-api/common';
 export * from 'ngx-ytd-api/search';
 export * from 'ngx-ytd-api/videos';
-export * from './version';
 // export * from './comments';
 // export * from './search';
 // export * from './videos';
